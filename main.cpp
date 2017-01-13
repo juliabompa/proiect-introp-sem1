c5f3281386c77dc6bab84889c4ca4df24cb4932b
juliabompa <juliabompa@users.noreply.github.com>
2017-01-13 23:13:55 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -27,11 +27,26 @@ unsigned char mine[20][34], numere[20][34], joc[20][34];
 int main()
 {
 	char pas;
-	int final;
+	int final = 0;
+
 	cout<<endl;
-	cout<<"		INSTRUCTIUNI JOC	";
+	cout<<"		HEY THERE!	";
 	cout<<endl<<endl;
-	cout<<" ";
+	cout<<"Instructiuni joc:";
+	cout<<endl<<"  -o miscare presupune introducerea a 3 date: ";
+	cout<<endl<<"	doua coordonate (linie si coloana) si o 'actiune'.";
+	cout<<endl<<"     -actiuni posibile: ";
+	cout<<endl<<"		 - s (descoperirea unei casute/click stanga), ";
+	cout<<endl<<"		 - f (stegulet/click dreapta), ";
+	cout<<endl<<"		 - d (descoperirea casutelor vecine/click stanga ";
+	cout<<endl<<"	           si click dreapta simultan).";
+	cout<<endl<<endl<<"Pentru inceperea jocului introduceti ";
+	cout<<endl<<"	litera corespunzatoare nivelului dorit:";
+	cout<<endl<<" b - beginner";
+	cout<<endl<<" i - intermediate";
+	cout<<endl<<" e - expert";
+	cout<<endl<<endl<<"	SUCCES!!!"<<endl;
+
 
 	cin>>pas;
 	while(pas!= 'b' && pas!= 'i' && pas!= 'e')
@@ -39,38 +54,38 @@ int main()
 
 	switch(pas)
 	{
-		case 'b':
-			construireMatrici(12, 13);
-			random(10, 9, 9);
-			Mine(12, 13);
-			afisare(12, 13);
-			final= desfasurareJoc(12, 13, 10);
-			if(final)
-				cout<<"		CONGRATULATIONS!!!	";
-			
-			break;
-		case 'i':
-			construireMatrici(19, 20);
-			random(40, 16, 16);
-			Mine(19, 20);
-			afisare(19, 20);
-			final=desfasurareJoc(19, 20, 40);
-			if(final)
-				cout<<"		CONGRATULATIONS!!!	";
-			
-			break;
-		case 'e':
-			construireMatrici(19, 34);
-			random(99, 16, 30);
-			Mine(19, 34);
-			afisare(19, 34);
-			final=desfasurareJoc(19, 34, 99);
-			if(final)
-				cout<<"		CONGRATULATIONS!!!	";
-			
-			break;
-		default:
-			cout<<"blabla";
+	case 'b':
+		construireMatrici(12, 13);
+		random(10, 9, 9);
+		Mine(12, 13);
+		afisare(12, 13);
+		final= desfasurareJoc(12, 13, 10);
+		if(final)
+			cout<<endl<<endl<<"		CONGRATULATIONS!!!	";
+
+		break;
+	case 'i':
+		construireMatrici(19, 20);
+		random(40, 16, 16);
+		Mine(19, 20);
+		afisare(19, 20);
+		final=desfasurareJoc(19, 20, 40);
+		if(final)
+			cout<<endl<<endl<<"		CONGRATULATIONS!!!	";
+
+		break;
+	case 'e':
+		construireMatrici(19, 34);
+		random(99, 16, 30);
+		Mine(19, 34);
+		afisare(19, 34);
+		final=desfasurareJoc(19, 34, 99);
+		if(final)
+			cout<<endl<<endl<<"		CONGRATULATIONS!!!	";
+
+		break;
+	default:
+		cout<<"blabla";
 	}
 	system("pause");
 	return 0;
@@ -108,46 +123,46 @@ void construireMatrici(int a, int b)
 			mine[i][j]='0';
 			joc[i][j]=254;
 		}
-	mine[0][0]=mine[0][1]=mine[1][0]=mine[1][1]=' ';
-	mine[a-1][0]=mine[a-1][1]=' ';
-	mine[0][b-1]=mine[0][2]=' ';
-	joc[0][0]=joc[0][1]=joc[1][0]=joc[1][1]=' ';
-	joc[a-1][0]=joc[a-1][1]=' ';
-	joc[0][b-1]=joc[0][2]=' ';
+		mine[0][0]=mine[0][1]=mine[1][0]=mine[1][1]=' ';
+		mine[a-1][0]=mine[a-1][1]=' ';
+		mine[0][b-1]=mine[0][2]=' ';
+		joc[0][0]=joc[0][1]=joc[1][0]=joc[1][1]=' ';
+		joc[a-1][0]=joc[a-1][1]=' ';
+		joc[0][b-1]=joc[0][2]=' ';
 
-	mine[1][2]=218;
-	mine[a-1][b-1]=217;
-	mine[a-1][2]=192;
-	mine[1][b-1]=191;
-	joc[1][2]=218;
-	joc[a-1][b-1]=217;
-	joc[a-1][2]=192;
-	joc[1][b-1]=191;
-			
-	for(i=3;i<b-1;i++)
-	{
-		if(i<29)
-			mine[0][i]=joc[0][i]='a' - 3 +i;
-		else 
-			mine[0][i]=joc[0][i]='A' + (i-29);
-		mine[1][i]=joc[1][i]='-';
-		mine[a-1][i]=joc[a-1][i]='-';
-	}
-	for(i=2;i<a-1;i++)
-	{
-		if(i<11)
+		mine[1][2]=218;
+		mine[a-1][b-1]=217;
+		mine[a-1][2]=192;
+		mine[1][b-1]=191;
+		joc[1][2]=218;
+		joc[a-1][b-1]=217;
+		joc[a-1][2]=192;
+		joc[1][b-1]=191;
+
+		for(i=3;i<b-1;i++)
 		{
-			mine[i][0]=joc[i][0]=' ';
-			mine[i][1]=joc[i][1]='1' - 2 + i;
+			if(i<29)
+				mine[0][i]=joc[0][i]='a' - 3 +i;
+			else 
+				mine[0][i]=joc[0][i]='A' + (i-29);
+			mine[1][i]=joc[1][i]='-';
+			mine[a-1][i]=joc[a-1][i]='-';
 		}
-		else
+		for(i=2;i<a-1;i++)
 		{
-			mine[i][0]=joc[i][0]='1';
-			mine[i][1]=joc[i][1]='0' - 11 + i;
+			if(i<11)
+			{
+				mine[i][0]=joc[i][0]=' ';
+				mine[i][1]=joc[i][1]='1' - 2 + i;
+			}
+			else
+			{
+				mine[i][0]=joc[i][0]='1';
+				mine[i][1]=joc[i][1]='0' - 11 + i;
+			}
+			mine[i][2]=joc[i][2]='|';
+			mine[i][b-1]=joc[i][b-1]='|';
 		}
-		mine[i][2]=joc[i][2]='|';
-		mine[i][b-1]=joc[i][b-1]='|';
-	}
 }
 
 void random(int numarBombe, int a, int b)
@@ -183,7 +198,7 @@ void Mine(int a, int b)
 
 int desfasurareJoc(int x, int y, int numarBombe)
 {
-	
+
 	int i, j, a, b, ok=1, flags, contor=0;
 
 	Mutare mutare;
@@ -192,13 +207,19 @@ int desfasurareJoc(int x, int y, int numarBombe)
 	{
 		do
 		{
-			cin>>mutare.primaCoordonata>>mutare.aDouaCoordonata>>mutare.actiune;
+
+			cin>>mutare.primaCoordonata;
+			cin>>mutare.aDouaCoordonata;
+			cin>>mutare.actiune;
 			a=mutare.primaCoordonata+1; 
 			if(mutare.aDouaCoordonata>='a')
 				b=(int)mutare.aDouaCoordonata-'a'+3;
 			else
 				b=(int)mutare.aDouaCoordonata-'A' + 3 + 26;
-		}while(a<2||a>x-1||b<2||b>y-1 || (mutare.actiune!='s'&&mutare.actiune!='d'&&mutare.actiune!='f') 
+
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}while(a<2||a>x-1||b<2||b>y-1 ||(mutare.actiune!='s' && mutare.actiune!='d' && mutare.actiune!='f')
 			|| mutare.actiune=='s' && joc[a][b] != 254 
 			|| mutare.actiune=='f' && (joc[a][b] != 254 && joc[a][b]!='+') 
 			|| mutare.actiune=='d' && joc[a][b] == 254);
@@ -215,10 +236,10 @@ int desfasurareJoc(int x, int y, int numarBombe)
 						if(joc[i][j]=='+' && numere[i][j]!='*')
 							joc[i][j]='x';
 					}
-				afisare(x,y);
-				cout<<endl<<"	"<<"BANG BANG YOU'RE DEAD"<<endl<<endl;
-				
-				ok=0;
+					afisare(x,y);
+					cout<<endl<<"	"<<"BANG BANG YOU'RE DEAD"<<endl<<endl;
+
+					ok=0;
 			}
 			else
 				if(numere[a][b]>'0' && numere[a][b]<='9')
